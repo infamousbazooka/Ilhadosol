e76a9cb1667411f5244f19209defcd92626954c5
infamousbazooka <magnusfernandes1295@gmail.com>
2015-10-14 00:17:15 +0530

    Contact page donegit add .

--- js/main.js
+++ js/main.js
@@ -9,8 +9,10 @@ jQuery(document).ready(function($) {
 $(window).bind('scroll', function() {
 	if ($(window).scrollTop() > 100) {
 		$('.logo').css('display', 'none');
+		$('header').css('backgroundColor', 'rgba(34,34,34,.8)');
 	}
 	else {
 		$('.logo').css('display', 'inline-block');
+		$('header').css('backgroundColor', 'rgba(34,34,34,1)');
 	}
 });
