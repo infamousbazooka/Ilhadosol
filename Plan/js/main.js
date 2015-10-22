f3788a3dc7623b68f14d5c18caf1855b48a5573d
infamousbazooka <magnusfernandes1295@gmail.com>
2015-10-22 11:04:13 +0530

    New favicon added. minor bugs fixed

--- Plan/js/main.js
+++ Plan/js/main.js
@@ -8,3 +8,17 @@ $(window).resize(function(e) {
 	$('.one').css('line-height', $(window).height() + "px");
 	$('#map').css('min-height', $('.contact').height());
 });
+$(window).bind('scroll', function() {
+	if($(window).width() > 800){
+		if ($(window).scrollTop() > 100) {
+			$('.logo').css('display', 'none');
+			$('header').css('position', 'fixed');
+			$('header').css('backgroundColor', 'rgba(34,45,47,.8)');
+		}
+		else {
+			$('.logo').css('display', 'inline-block');
+			$('header').css('position', 'relative');
+			$('header').css('backgroundColor', 'rgba(34,45,47,1)');
+		}
+	}
+});
--- js/main.js
+++ js/main.js
@@ -3,16 +3,23 @@ jQuery(document).ready(function($) {
 	$('.one').css('line-height', $(window).height() + "px");
 	$('#map').css('min-height', $('.contact').height());
 });
+jQuery(window).resize(function($) {
+	$('.section').css('min-height', $(window).height());
+	$('.one').css('line-height', $(window).height() + "px");
+	$('#map').css('min-height', $('.contact').height());
+});
 
 $(window).bind('scroll', function() {
-	if ($(window).scrollTop() > 100) {
-		$('.logo').css('display', 'none');
-		$('header').css('position', 'fixed');
-		$('header').css('backgroundColor', 'rgba(34,45,47,.8)');
-	}
-	else {
-		$('.logo').css('display', 'inline-block');
-		$('header').css('position', 'relative');
-		$('header').css('backgroundColor', 'rgba(34,45,47,1)');
+	if($(window).width() > 800){
+		if ($(window).scrollTop() > 100) {
+			$('.logo').css('display', 'none');
+			$('header').css('position', 'fixed');
+			$('header').css('backgroundColor', 'rgba(34,45,47,.8)');
+		}
+		else {
+			$('.logo').css('display', 'inline-block');
+			$('header').css('position', 'relative');
+			$('header').css('backgroundColor', 'rgba(34,45,47,1)');
+		}
 	}
 });
